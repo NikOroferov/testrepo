c0b4de0f1938365bd231740b519677c660861b8b
Nikolay Yaryhin <77698141+NikOroferov@users.noreply.github.com>
2022-05-10 11:30:21 +0300

    Commit

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = ({ develop }) => ({
   mode: develop ? "development" : "production",
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
     clean: true,
   },
